47ff53b9d12ee59d6c28ed92b5cedbcc37810f34
smhrd <smhrd@DESKTOP-NQPB5SO>
2021-10-22 12:03:43 +0900

    탐험

--- tamagotchi/src/tamagotchi/adv.java
+++ tamagotchi/src/tamagotchi/adv.java
@@ -6,14 +6,7 @@ import java.util.Scanner;
 import java1020team.Book;
 
 public class adv {
-	
-	Random rd = new Random();// 랜덤 모험
-	Scanner sc = new Scanner();//입력
-	
-	int adv = 0;
-	int hp = 0;
-	int exp = 0;
-	
+		
 	int ran = rd.nextInt(3)+1;
 	
 	if(menu == 1) {// hp -30 exp +40
