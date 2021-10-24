0f23da2368ed1f13f4e15e2b8e4b8d9237d3e577
smhrd <smhrd@DESKTOP-TK2JIS7>
2021-10-24 15:31:33 +0900

    이미지 추가

---
+++ tamagotchi/src/tamagotchi/image.java
@@ -0,0 +1,179 @@
+package tamagotchi;
+
+public class image {
+
+	public static void main(String[] args) {
+		
+
+
+		if(lv=<1) {
+			System.out.println("         ,,!        \r\n"
+					+ "         .. ;       \r\n"
+					+ "         -@$        \r\n"
+					+ "         -@*        \r\n"
+					+ "         -@*        \r\n"
+					+ "         ,@*        \r\n"
+					+ "     ~*@@@@@@~      \r\n"
+					+ "    ,--=@@@@@@@,,   \r\n"
+					+ "   ;@#:.:@@@@:.-    \r\n"
+					+ "  ~@@=~.~@@@#; :$   \r\n"
+					+ "  $@#=*;*#@-  .!#-  \r\n"
+					+ "  @@@$==$@#==$#@;:  \r\n"
+					+ " *@@@@@@@#.*==$@@@  \r\n"
+					+ " $@@@@@@@@,    @@@  \r\n"
+					+ " $@@@@@@@@@   !@@@  \r\n"
+					+ " *@@@@@@@@@#*$@@@@  \r\n"
+					+ "  @@@@@@@@@@@@@@@#  \r\n"
+					+ "  *@@@@@$=====$@@~  \r\n"
+					+ "  -@@@@========$@-  \r\n"
+					+ "    @#==========-   \r\n"
+					+ "    ,$=========~    \r\n"
+					+ "      -$*****;");
+			
+			
+		}else if(lv<=10) {
+			System.out.println("<им))))><");
+			
+		}else if(lv<=20) {
+			System.out.println("\r\n"
+					+ "			\r\n"
+					+ "			System.out.println(\"   !* \");\r\n"
+					+ "			System.out.println(\"   #@ \");\r\n"
+					+ "			System.out.println(\"   ..                         ~##  \");\r\n"
+					+ "			System.out.println(\"              -;$@@@@#!:.   .=@@*\");\r\n"
+					+ "			System.out.println(\"             $@@=-..-!@@@;  $@-@: \");\r\n"
+					+ "			System.out.println(\"    -$~    ~#$~        ,;@$!#.:@.\");\r\n"
+					+ "			System.out.println(\"    ~@;   :@* ,-         .#@! !# \");\r\n"
+					+ "			System.out.println(\"     ,   ,@;  #@.         ~@- $:\");\r\n"
+					+ "			System.out.println(\"         ~@-  ;;          -@- #~\");\r\n"
+					+ "			System.out.println(\"          =@,             =@; *= \");\r\n"
+					+ "			System.out.println(\"           *@;          ,=#$$ ;@ \");\r\n"
+					+ "			System.out.println(\"            ;@@;      -#@#. @=-@-\");\r\n"
+					+ "			System.out.println(\"             .:#@@$#@@@*~   -@$@; \");\r\n"
+					+ "			System.out.println(\"                 -::-        .=##  \");\r\n"
+					+ "			System.out.println(\"                               .,     \");\r\n"
+					+ "		\r\n"
+					+ "			\r\n"
+					+ "		");
+			
+		}else if(lv<=30) {
+			System.out.println("	\r\n"
+					+ "	System.out.println(\"                      ~,      \");\r\n"
+					+ "	System.out.println(\"                     - -\");\r\n"
+					+ "	System.out.println(\"                    ,  ;      \");\r\n"
+					+ "	System.out.println(\"                   :  ,;\");\r\n"
+					+ "	System.out.println(\"                  :  .-;\");\r\n"
+					+ "	System.out.println(\"                 !,--~;;\");\r\n"
+					+ "	System.out.println(\"            !!!!    ;;;!\");\r\n"
+					+ "	System.out.println(\"         .;.            ..; \");\r\n"
+					+ "	System.out.println(\"         .;.            ..; \");\r\n"
+					+ "	System.out.println(\"     ~.      ,~~          .-;\");\r\n"
+					+ "	System.out.println(\"    -        ---;-        .,,,\");\r\n"
+					+ "	System.out.println(\" !. .   @#  ! ~ ;!         ,--\");\r\n"
+					+ "	System.out.println(\"..!!    $=  ! ,.~;        ,,~-\");\r\n"
+					+ "	System.out.println(\".,          ! ,-;;       ,--;,\");\r\n"
+					+ "	System.out.println(\" :            --;! .. . .-,,-:\");\r\n"
+					+ "	System.out.println(\" , ..    ... .!;      ..,,-,-;\");\r\n"
+					+ "	System.out.println(\" -   .         ..   ...-,,,-~~\");\r\n"
+					+ "	System.out.println(\" ;,.      ...........,,----,~,\");\r\n"
+					+ "	System.out.println(\" ;;!.           ,.----,,,--.--\");\r\n"
+					+ "	System.out.println(\" :!~-.          ...........,:,\");\r\n"
+					+ "	System.out.println(\"    !......................~!\");\r\n"
+					+ "	System.out.println(\"     ;~.................~~~~:\");\r\n"
+					+ "	System.out.println(\"       ~~~.........~~~~~~~~~~\");\r\n"
+					+ "	System.out.println(\"        ..!:~:~~~~~~~~~~~~;.\");\r\n"
+					+ "	System.out.println(\"           ,-~;;;;;;;;;;;, \");\r\n"
+					+ "	System.out.println(\"                  ~~~~;;; \");\r\n"
+					+ "	System.out.println(\"                   :. ~;;\");\r\n"
+					+ "	System.out.println(\"                    ; .;:\");\r\n"
+					+ "	System.out.println(\"                     -.;\");\r\n"
+					+ "	System.out.println(\"                      ~!\");\r\n"
+					+ "		");
+			
+		}else if(die) {
+			System.out.println("\r\n"
+					+ "			System.out.println(\"                         .\");\r\n"
+					+ "			System.out.println(\"                   .$@######@$.\");\r\n"
+					+ "			System.out.println(\"                .,=#=.      .##=,. \");\r\n"
+					+ "			System.out.println(\"               .=#-            -#=.\");\r\n"
+					+ "			System.out.println(\"              .=$-              ,$*.\");\r\n"
+					+ "			System.out.println(\"              !$-                ,==,\");\r\n"
+					+ "			System.out.println(\"             ,=~                  -$*.\");\r\n"
+					+ "			System.out.println(\"             !=,                   -*~  \");\r\n"
+					+ "			System.out.println(\"            -=:                     ;; \");\r\n"
+					+ "			System.out.println(\"            ;=,                     ;=- \");\r\n"
+					+ "			System.out.println(\"            ;;   :!!!!      !!!!:   -=;~!!!!. \");\r\n"
+					+ "			System.out.println(\"    .!!!!~  ;;  :#@@@@*    *@@@@#:   ;**!~~$*.\");\r\n"
+					+ "			System.out.println(\"   .=$~~!#~ ;;  *@@@@@@*  *@@@@@@#:  ;#;.  -$,\");\r\n"
+					+ "			System.out.println(\"   .#,  .;! ;; :#@@@@@@@  @@@@@@@@!  ;@:    =,\");\r\n"
+					+ "			System.out.println(\"   .$    ~! ;; !@@@@@@@@  @@@@@@@@!  ;@!.   =$=.\");\r\n"
+					+ "			System.out.println(\"   .$   -#! ;; .=@@@@@@@  @@@@@@@@! ~#@#~   .,#$\");\r\n"
+					+ "			System.out.println(\"   .@*  ~@@$#;  *@@@@@@,  ,@@@@@@*  :@;.      .# \");\r\n"
+					+ "			System.out.println(\"  .@$      ;@@:  =@@@@,    .@@@@=   :;         # \");\r\n"
+					+ "			System.out.println(\"  .$         !:                    :@;  !@~   $@ \");\r\n"
+					+ "			System.out.println(\"  .$    ~=,  ;@~                   :!~$#$-*@-$@.\");\r\n"
+					+ "			System.out.println(\"  .$..  ~###!.*:        @@@       :@@#:    *##.  \");\r\n"
+					+ "			System.out.println(\"   =$=,;!. ~$$#@:       @@#      :@$: \");\r\n"
+					+ "			System.out.println(\"    .==;      :$@:      ===     :@;\");\r\n"
+					+ "			System.out.println(\"                ;#~            -#; \");\r\n"
+					+ "			System.out.println(\"               ,~$!.           -$~~,       -~.\");\r\n"
+					+ "			System.out.println(\"     ~~-     ,~==#!.-. ~  ~  ~ -#!==~,   -:=$!~\");\r\n"
+					+ "			System.out.println(\"   :;$*$-  ,:==!,=!.$, @  @ .$ -= ,!=*::,**~~;#.\");\r\n"
+					+ "			System.out.println(\"  .#;-.*$:;;;;,  =!.$, @  @ .$ -=   ,;;$=!,   $; \");\r\n"
+					+ "			System.out.println(\"  !*   -@=:-   ~;#*.$, @  @ .$ -#;~    -:,    $@\");\r\n"
+					+ "			System.out.println(\"  #,   .:-   ~!$!$#,$, @  @ .$ -$:;!!-        $: \");\r\n"
+					+ "			System.out.println(\" *!      ,;*=!~.-$=@, @  @ .$.== ,~*$*!,    !: \");\r\n"
+					+ "			System.out.println(\"  ,$*!.  .;*--.   ,$@==@==@==@=$,   .--*$-   :* \");\r\n"
+					+ "			System.out.println(\"   ,~$    ;;       ,-----------,       .=~    #.\");\r\n"
+					+ "			System.out.println(\"    ,=    :;                            !~   *@.\");\r\n"
+					+ "			System.out.println(\"    ,@*   :;                            !#- ,$- \");\r\n"
+					+ "			System.out.println(\"     -@$=#!                              *###- \");\r\n"
+					+ "			System.out.println(\"      -@#;     \");												\r\n"
+					+ "		");
+		}else if(conin==1) {
+			System.out.println("\"               ...                ...             \\n\"\r\n"
+					+ "					+ \"            ........           .........          \\n\"\r\n"
+					+ "					+ \"          ............        ............        \\n\"\r\n"
+					+ "					+ \"         ..............      ..............       \\n\"\r\n"
+					+ "					+ \"        ................   .................      \\n\"\r\n"
+					+ "					+ \"        ................. ..................      \\n\"\r\n"
+					+ "					+ \"       ......................................     \\n\"\r\n"
+					+ "					+ \"       ......................................     \\n\"\r\n"
+					+ "					+ \"       ......................................     \\n\"\r\n"
+					+ "					+ \"      .......................................     \\n\"\r\n"
+					+ "					+ \"      .......................................     \\n\"\r\n"
+					+ "					+ \"      .......................................     \\n\"\r\n"
+					+ "					+ \"       ......................................     \\n\"\r\n"
+					+ "					+ \"       ......................................     \\n\"\r\n"
+					+ "					+ \"       ......................................     \\n\"\r\n"
+					+ "					+ \"       .....................................      \\n\"\r\n"
+					+ "					+ \"        ....................................      \\n\"\r\n"
+					+ "					+ \"        ...................................       \\n\"\r\n"
+					+ "					+ \"        ...................................       \\n\"\r\n"
+					+ "					+ \"         .................................        \\n\"\r\n"
+					+ "					+ \"          ................................        \\n\"\r\n"
+					+ "					+ \"           ..............................         \\n\"\r\n"
+					+ "					+ \"            ............................          \\n\"\r\n"
+					+ "					+ \"             ..........................           \\n\"\r\n"
+					+ "					+ \"              ........................            \\n\"\r\n"
+					+ "					+ \"               ......................             \\n\"\r\n"
+					+ "					+ \"                ....................              \\n\"\r\n"
+					+ "					+ \"                ...................               \\n\"\r\n"
+					+ "					+ \"                  ................                \\n\"\r\n"
+					+ "					+ \"                    ............                  \\n\"\r\n"
+					+ "					+ \"                     ..........                   \\n\"\r\n"
+					+ "					+ \"                       ......                     \\n\"\r\n"
+					+ "					+ \"                        ...                       \\n\"\r\n"
+					+ "					+ \"                         .  \"");
+			
+		
+		// TODO Auto-generated method stub
+
+	}
+
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
